9f0d1882fd8e5c1e3c5468d0af9aeda64e5c96b4
Jamestig <tim.j.ringuet@gmail.com>
2018-07-25 13:11:12 +1000

    jq menu

--- scripts/main.js
+++ scripts/main.js
@@ -73,13 +73,54 @@ $(document).ready(function () {
 	var thisPage = $(location).attr('href');
 	var currentPage;
 
-	scrollLink.click(function (e) {
-		e.preventDefault();
-		$('body,html').animate({
-			scrollTop: $(this.hash).offset().top
-		}, 1000);
-	}); // End smooth scrolling function
+	if (currentPage === 'index.html' || currentPage === 'story.html' || currentPage === 'bnb.html') {
+		scrollLink.click(function (e) {
+			e.preventDefault();
+			$('body,html').animate({
+				scrollTop: $(this.hash).offset().top
+			}, 1000);
+		}); // End smooth scrolling function
+	};
 
+
+	// Accordion
+
+	var accordionHidden = $('.accordionInner');
+	var pageLinks = $('.accordion > li > a');
+	var menuLinks = $('#inner-restaurant > li > a');
+	var current_href = $(location).attr('href');
+	var currentPage = $(location).attr('href').split('/')[3];
+	var prevLink = $('.accordionInner').prev('a');
+	var link_href = prevLink.attr('href');
+	var openHeader = $('a[href="' + currentPage + '"]');
+
+	console.log(currentPage);
+	//console.log(accordionHidden);
+	//console.log(pageLinks);
+	//console.log(prevLink);
+	//console.log(link_href);
+
+	if (window.matchMedia('(min-width: 1280px)').matches) {
+
+		for (x = 0; x < prevLink.length; x++) {
+			var findLink = prevLink.eq(x).attr('href');
+			var openMenu = $('a[href="' + findLink + '"]').next('ul');
+			var openList = openMenu.find('a');
+			var menuLinks = current_href.indexOf('menu-');
+
+			if (prevLink.eq(x).attr('href') === currentPage) {
+				openMenu.slideDown();
+				openList.addClass('scroll');
+			}
+
+			if (menuLinks > -1) {
+				$('#inner-restaurant').show();
+				$('#restaurantLink').addClass('activePage');
+			}
+		}
+
+		openHeader.addClass('activePage');
+	}
 
 	// Active link switching and background fading
 	var sectionOffset;
@@ -102,23 +143,23 @@ $(document).ready(function () {
 					$(this).parent().addClass('activeSection');
 					$(this).parent().siblings().removeClass('activeSection');
 				}
-/*
-				if (scrollbarLocation <= homeHeight) {
-					$('#homeRestaurantBG').fadeIn(1000);
-					$('#homeCafeBG').fadeOut(1000);
-					pageSection = "sectionOne";
-				} else if (scrollbarLocation > homeHeight && scrollbarLocation <= homeHeight * 2) {
-					$('#homeCafeBG').fadeIn(1000);
-					$('#homeRestaurantBG').fadeOut(1000);
-					pageSection = "sectionTwo";
-				} else if (scrollbarLocation > homeHeight * 2 && scrollbarLocation <= homeHeight * 3) {
-					pageSection = "sectionThree";
-				} else if (scrollbarLocation > homeHeight * 3 && scrollbarLocation <= homeHeight * 4) {
-					pageSection = "sectionFour";
-				} else if (scrollbarLocation > homeHeight * 4 && scrollbarLocation <= homeHeight * 5) {
-					pageSection = "sectionFive";
-				}
-*/
+				/*
+								if (scrollbarLocation <= homeHeight) {
+									$('#homeRestaurantBG').fadeIn(1000);
+									$('#homeCafeBG').fadeOut(1000);
+									pageSection = "sectionOne";
+								} else if (scrollbarLocation > homeHeight && scrollbarLocation <= homeHeight * 2) {
+									$('#homeCafeBG').fadeIn(1000);
+									$('#homeRestaurantBG').fadeOut(1000);
+									pageSection = "sectionTwo";
+								} else if (scrollbarLocation > homeHeight * 2 && scrollbarLocation <= homeHeight * 3) {
+									pageSection = "sectionThree";
+								} else if (scrollbarLocation > homeHeight * 3 && scrollbarLocation <= homeHeight * 4) {
+									pageSection = "sectionFour";
+								} else if (scrollbarLocation > homeHeight * 4 && scrollbarLocation <= homeHeight * 5) {
+									pageSection = "sectionFive";
+								}
+				*/
 			});
 
 
@@ -130,70 +171,4 @@ $(document).ready(function () {
 		} // Match query if statement
 	}); // End scroll function
 
-	// Accordion
-
-	var accordionHidden = $('.accordionInner');
-	var pageLinks = $('.accordion > li > a');
-	var menuLinks = $('#inner-restaurant > li > a');
-	var current_href = $(location).attr('href');
-	var currentPage = $(location).attr('href').split('/')[3];
-	var prevLink = $('.accordionInner').prev('a');
-	var link_href = prevLink.attr('href');
-
-	//console.log(currentPage);
-	//console.log(accordionHidden);
-	//console.log(pageLinks);
-	//console.log(menuLinks);
-	//console.log(prevLink);
-	//console.log(link_href);
-
-	for (x = 0; x < prevLink.length; x++) {
-		var findLink = prevLink.eq(x).attr('href');
-		var openMenu = $('a[href="' + findLink + '"]').next('ul');
-
-		if (prevLink.eq(x).attr('href') === currentPage) {
-			openMenu.slideDown();
-		}
-	}
-
-	/*
-		if ((current_href).indexOf('index.html') > -1) {
-			$('#inner-home').slideDown();
-			$(pageLinks[0]).addClass('activePage');
-		} else if ((current_href).indexOf('story.html') > -1) {
-			$('#inner-story').slideDown();
-			$(pageLinks[1]).addClass('activePage');
-		} else if ((current_href).indexOf('bnb.html') > -1) {
-			$('#inner-bnb').slideDown();
-			$(pageLinks[2]).addClass('activePage');
-		} else if ((current_href).indexOf('reservations.html') > -1) {
-			$(pageLinks[3]).addClass('activePage');
-		} else if ((current_href).indexOf('menu-') > -1) {
-			$('#inner-restaurant').slideDown();
-			$(pageLinks[4]).addClass('activePage');
-		} else if ((current_href).indexOf('menu-restaurant.html') > -1) {
-			$(pageLinks[4]).addClass('activePage');
-		}
-	*/
-
-	/*
-	$('.toggle').click(function (e) {
-
-		if (window.matchMedia('(min-width: 1280px)').matches) {
-			e.preventDefault();
-
-			var $this = $(this);
-
-			if ($this.next().hasClass('show')) {
-				$this.next().removeClass('show');
-				$this.next().slideUp(350);
-			} else {
-				$this.parent().parent().find('li .accordionInner').removeClass('show');
-				$this.parent().parent().find('li .accordionInner').slideUp(350);
-				$this.next().toggleClass('show');
-				$this.next().slideToggle(350);
-			};
-		} // Match query if statement
-	}); // End accordion function
-	*/
 });
